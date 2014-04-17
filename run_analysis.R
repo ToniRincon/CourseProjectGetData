abdbd9f16fb47070b2f96de44a2575b6c6848c37
tonirincon <antonio.rincon@gmail.com>
2014-04-17 10:13:04 +0200

    run_analysis.R and CodeBook.md template

---
+++ run_analysis.R
@@ -0,0 +1,47 @@
+library(reshape2)
+run_analysis <- function() {
+  
+  # read features.txt
+  features_labels = read.table(file="features.txt", col.names=c("feature_id","feature"))
+  # clean features labels
+  features_labels$feature = gsub("[,-]",".",gsub("[()]","",features_labels$feature))
+  
+  # read activity_labels.txt
+  activity_labels = read.table(file="activity_labels.txt", col.names=c("activity_id","activity"))
+  
+  # read subject_train.txt and subject_test.txt to be combined
+  subjects_train = read.table(file="train/subject_train.txt", col.names=c("subject"))
+  subjects_test = read.table(file="test/subject_test.txt", col.names=c("subject"))
+  subjects = rbind(subjects_train,subjects_test)
+  rm(subjects_train)
+  rm(subjects_test)
+  
+  # read X_train.txt and X_test.txt to be combined
+  # features names are taken from features
+  features_train = read.table(file="train/X_train.txt", col.names=features_labels$feature)
+  features_test = read.table(file="test/X_test.txt", col.names=features_labels$feature)
+  features = rbind(features_train,features_test)
+  rm(features_train)
+  rm(features_test)
+  
+  # read y_train.txt and y_test.txt to be combined
+  activities_train = read.table(file="train/y_train.txt", col.names=c("activity_id"))
+  activities_test = read.table(file="test/y_test.txt", col.names=c("activity_id"))
+  activities = rbind(activities_train,activities_test)
+  rm(activities_train)
+  rm(activities_test)
+  
+  # select only -mean() and -std() features
+  features_labels=features_labels[grep("\\.(mean|std)\\b",features_labels$feature),"feature"]
+  features = features[,features_labels]
+  
+  # merge activity names and activity labels deleting activity_id
+  # combine subjects, activities and features
+  activities = merge(activities,activity_labels)
+  activities$activity_id = NULL
+  features = cbind(subjects,activities,features)
+  
+  # finally melt variables using subject and activity, and aggrefate using mean
+  features_melted = melt(features,id=c("subject","activity"))
+  features_tidy = dcast(features_melted,subject+activity~variable,mean)
+}  
